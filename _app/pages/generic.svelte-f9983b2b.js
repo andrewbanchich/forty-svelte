0b14fe0396cd4f74996668b5919ed37d6a5d5ec8
andrewbanchich <13824577+andrewbanchich@users.noreply.github.com>
2021-11-14 20:15:52 +0000

    Deploying to gh-pages from @ andrewbanchich/forty-svelte@79cbebe7b84abebe9c7013bee72ce60c487f58ec 🚀

--- _app/pages/generic.svelte-f9983b2b.js
+++ _app/pages/generic.svelte-f9983b2b.js
@@ -1,4 +1,4 @@
-import{S as w,i as z,s as B,e as s,t as b,k as P,c as r,a as u,g as _,d as n,n as E,b as l,L as F,f as J,D as t,E as L}from"../chunks/vendor-fe5a5cb5.js";function K(T){let i,m,e,o,p,x,D,c,d,M,I,v,N,C,h,G,S,f,j;return{c(){i=s("div"),m=s("section"),e=s("div"),o=s("header"),p=s("h1"),x=b("Generic"),D=P(),c=s("span"),d=s("img"),I=P(),v=s("p"),N=b(`Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus.\r
+import{S as w,i as z,s as B,e as s,t as b,k as P,c as r,a as u,g as _,d as n,n as E,b as l,M as F,f as J,D as t,E as j}from"../chunks/vendor-008806b1.js";function K(T){let i,m,e,o,p,x,D,c,d,k,I,v,N,C,h,G,M,f,S;return{c(){i=s("div"),m=s("section"),e=s("div"),o=s("header"),p=s("h1"),x=b("Generic"),D=P(),c=s("span"),d=s("img"),I=P(),v=s("p"),N=b(`Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus.\r
 				Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam,\r
 				vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet,\r
 				fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in\r
@@ -10,11 +10,11 @@ import{S as w,i as z,s as B,e as s,t as b,k as P,c as r,a as u,g as _,d as n,n a
 				venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in\r
 				lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis\r
 				egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus,\r
-				vel sed vehicula.`),S=P(),f=s("p"),j=b(`Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor\r
+				vel sed vehicula.`),M=P(),f=s("p"),S=b(`Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor\r
 				imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit\r
 				amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget\r
 				turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit tristique lorem\r
-				ipsum dolor.`),this.h()},l(q){i=r(q,"DIV",{id:!0,class:!0});var g=u(i);m=r(g,"SECTION",{id:!0});var k=u(m);e=r(k,"DIV",{class:!0});var a=u(e);o=r(a,"HEADER",{class:!0});var y=u(o);p=r(y,"H1",{});var A=u(p);x=_(A,"Generic"),A.forEach(n),y.forEach(n),D=E(a),c=r(a,"SPAN",{class:!0});var H=u(c);d=r(H,"IMG",{src:!0,alt:!0}),H.forEach(n),I=E(a),v=r(a,"P",{});var V=u(v);N=_(V,`Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus.\r
+				ipsum dolor.`),this.h()},l(q){i=r(q,"DIV",{id:!0,class:!0});var g=u(i);m=r(g,"SECTION",{id:!0});var y=u(m);e=r(y,"DIV",{class:!0});var a=u(e);o=r(a,"HEADER",{class:!0});var A=u(o);p=r(A,"H1",{});var H=u(p);x=_(H,"Generic"),H.forEach(n),A.forEach(n),D=E(a),c=r(a,"SPAN",{class:!0});var L=u(c);d=r(L,"IMG",{src:!0,alt:!0}),L.forEach(n),I=E(a),v=r(a,"P",{});var V=u(v);N=_(V,`Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus.\r
 				Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam,\r
 				vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet,\r
 				fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in\r
@@ -26,8 +26,8 @@ import{S as w,i as z,s as B,e as s,t as b,k as P,c as r,a as u,g as _,d as n,n a
 				venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in\r
 				lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis\r
 				egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus,\r
-				vel sed vehicula.`),O.forEach(n),S=E(a),f=r(a,"P",{});var R=u(f);j=_(R,`Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor\r
+				vel sed vehicula.`),O.forEach(n),M=E(a),f=r(a,"P",{});var R=u(f);S=_(R,`Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor\r
 				imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit\r
 				amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget\r
 				turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit tristique lorem\r
-				ipsum dolor.`),R.forEach(n),a.forEach(n),k.forEach(n),g.forEach(n),this.h()},h(){l(o,"class","major"),F(d.src,M="images/pic11.jpg")||l(d,"src",M),l(d,"alt",""),l(c,"class","image main"),l(e,"class","inner"),l(m,"id","one"),l(i,"id","main"),l(i,"class","alt")},m(q,g){J(q,i,g),t(i,m),t(m,e),t(e,o),t(o,p),t(p,x),t(e,D),t(e,c),t(c,d),t(e,I),t(e,v),t(v,N),t(e,C),t(e,h),t(h,G),t(e,S),t(e,f),t(f,j)},p:L,i:L,o:L,d(q){q&&n(i)}}}class U extends w{constructor(i){super();z(this,i,null,K,B,{})}}export{U as default};
+				ipsum dolor.`),R.forEach(n),a.forEach(n),y.forEach(n),g.forEach(n),this.h()},h(){l(o,"class","major"),F(d.src,k="images/pic11.jpg")||l(d,"src",k),l(d,"alt",""),l(c,"class","image main"),l(e,"class","inner"),l(m,"id","one"),l(i,"id","main"),l(i,"class","alt")},m(q,g){J(q,i,g),t(i,m),t(m,e),t(e,o),t(o,p),t(p,x),t(e,D),t(e,c),t(c,d),t(e,I),t(e,v),t(v,N),t(e,C),t(e,h),t(h,G),t(e,M),t(e,f),t(f,S)},p:j,i:j,o:j,d(q){q&&n(i)}}}class U extends w{constructor(i){super();z(this,i,null,K,B,{})}}export{U as default};
