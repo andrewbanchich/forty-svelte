96b478f60ccf15e67a5cd0ad2ed548cfee70b54c
Andrew Banchich <andrewbanchich@protonmail.ch>
2021-11-14 14:23:52 -0500

    WIP

--- svelte.config.js
+++ svelte.config.js
@@ -6,13 +6,9 @@ const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: preprocess(),
-
   kit: {
     adapter: adapter(),
     appDir: 'forty-svelte',
-    paths: {
-      base: '/forty-svelte'
-    },
     ssr: false,
     target: '#svelte'
   }
