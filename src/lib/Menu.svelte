a12d8c2f6081c8fae6b4d116d157a279d8121135
Andrew Banchich <andrewbanchich@protonmail.ch>
2021-11-28 13:43:19 -0500

    WIP

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -5,70 +5,62 @@
 
 <nav id="menu" transition:fade={{ duration: 150 }}>
 	<div class="inner">
-		<ul class="links">
-			<li><a href="/">Home</a></li>
-			<li><a href="/landing">Landing</a></li>
-			<li><a href="/generic">Generic</a></li>
-			<li><a href="/elements">Elements</a></li>
+		<ul class="links" on:click={() => (open = false)}>
+			<li>
+				<a href="/">Home</a>
+			</li>
+			<li>
+				<a href="/landing">Landing</a>
+			</li>
+			<li>
+				<a href="/generic">Generic</a>
+			</li>
+			<li>
+				<a href="/elements">Elements</a>
+			</li>
 		</ul>
 
-		<ul class="actions stacked">
-			<li><a href="#" class="button primary fit">Get Started</a></li>
-			<li><a href="#" class="button fit">Log In</a></li>
+		<ul id="actions">
+			<li>
+				<a class="button primary fit">Get Started</a>
+			</li>
+			<li>
+				<a class="button fit">Log In</a>
+			</li>
 		</ul>
 	</div>
 </nav>
 
 <style>
 	#menu {
-		transition: transform 0.35s ease, opacity 0.35s ease, visibility 0.35s;
 		align-items: center;
-		display: -moz-flex;
-		display: -webkit-flex;
-		display: -ms-flex;
 		display: flex;
 		justify-content: center;
-		pointer-events: none;
 		background: rgba(36, 41, 67, 0.9);
 		box-shadow: none;
 		height: 100%;
 		left: 0;
-		/* opacity: 0; */
 		overflow: hidden;
 		padding: 3em 2em;
 		position: fixed;
 		top: 0;
-		/* visibility: hidden; */
 		width: 100%;
 		backdrop-filter: blur(0.5em);
-
-		/* z-index: 3; */
 	}
 
-	#menu .inner {
-		-moz-transition: -moz-transform 0.35s ease-out, opacity 0.35s ease, visibility 0.35s;
-		-webkit-transition: -webkit-transform 0.35s ease-out, opacity 0.35s ease, visibility 0.35s;
-		-ms-transition: -ms-transform 0.35s ease-out, opacity 0.35s ease, visibility 0.35s;
-		transition: transform 0.35s ease-out, opacity 0.35s ease, visibility 0.35s;
-		-moz-transform: rotateX(20deg);
-		-webkit-transform: rotateX(20deg);
-		-ms-transform: rotateX(20deg);
-		transform: rotateX(20deg);
-		-webkit-overflow-scrolling: touch;
+	.inner {
 		max-width: 100%;
 		max-height: 100vh;
-		/* opacity: 0; */
 		overflow: auto;
 		text-align: center;
-		/* visibility: hidden; */
 		width: 18em;
 	}
 
-	#menu .inner > :first-child {
+	.inner > :first-child {
 		margin-top: 2em;
 	}
 
-	#menu .inner > :last-child {
+	.inner > :last-child {
 		margin-bottom: 3em;
 	}
 
@@ -82,7 +74,7 @@
 		padding: 0;
 	}
 
-	#menu ul.links > li > a:not(.button) {
+	ul.links > li > a:not(.button) {
 		border: 0;
 		border-top: solid 1px rgba(212, 212, 255, 0.1);
 		display: block;
@@ -94,27 +86,11 @@
 		text-transform: uppercase;
 	}
 
-	#menu ul.links > li > .button {
-		display: block;
-		margin: 0.5em 0 0 0;
-	}
-
-	#menu ul.links > li:first-child > a:not(.button) {
+	ul.links > li:first-child > a:not(.button) {
 		border-top: 0 !important;
 	}
 
-	#menu {
-		pointer-events: auto;
-		opacity: 1;
-		visibility: visible;
-	}
-
-	#menu .inner {
-		-moz-transform: none;
-		-webkit-transform: none;
-		-ms-transform: none;
-		transform: none;
-		opacity: 1;
-		visibility: visible;
+	#actions li {
+		margin-bottom: 1.3rem;
 	}
 </style>
